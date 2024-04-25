4a0c61910c8ba37183f8ea21a4172027a70192fd
zile028 <zile028@gmail.com>
2024-04-25 16:06:19 +0200

    index fix 08

--- Core/constants.php
+++ Core/constants.php
@@ -5,3 +5,8 @@ const ADRESA = "11080 Београд, ул. Градски парк 2";
 const TELEFON = "(+381) 11 3771-552";
 const EMAIL = "info@vpscacak.edu.rs";
 const RADNO_VREME = "09:00 - 17:00";
+
+const MAIL_USERNAME = "fsdev1234@gmail.com";
+const MAIL_PASSWORD = "emvsmefrteryzran";
+const MAIL_HOST = "smtp.gmail.com";
+const MAIL_PORT = 587;
