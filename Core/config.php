f3e07b976e9b6e8114ff9e82f5ae3aa1836208ac
zile028 <zile028@gmail.com>
2024-04-29 07:46:26 +0200

    refactor config

--- Core/config.php
+++ Core/config.php
@@ -1,7 +1,7 @@
 <?php
-dd($_SERVER["SERVER_NAME"]);
-return $_SERVER["SERVER_NAME"] === "localhost" ?
-    [
+
+$serverConfig = [
+    "dev" => [
         "database" => [
             "host" => "localhost",
             "port" => 3306,
@@ -9,7 +9,19 @@ return $_SERVER["SERVER_NAME"] === "localhost" ?
             "charset" => "utf8mb4",
             "username" => "root",
             "password" => ""
-        ]] : [
+        ]
+    ],
+    "test" => [
+        "database" => [
+            "host" => "localhost",
+            "port" => 3306,
+            "dbname" => "zilecom_vps_sajt",
+            "charset" => "utf8mb4",
+            "username" => "zilecom_vpsadmin",
+            "password" => "LbM0uvD=3JvN"
+        ]
+    ],
+    "production" => [
         "database" => [
             "host" => "localhost",
             "port" => 3306,
@@ -18,4 +30,38 @@ return $_SERVER["SERVER_NAME"] === "localhost" ?
             "username" => "vpscacak_vps_admin",
             "password" => "R!1kQaJUi%U;"
         ]
-    ];
+    ]
+];
+
+switch ($_SERVER["SERVER_NAME"]) {
+    case "localhost":
+        return $serverConfig["dev"];
+        break;
+    case "vps.zile028.com":
+        return $serverConfig["test"];
+        break;
+    default:
+        return $serverConfig["production"];
+        break;
+}
+
+//
+//return $_SERVER["SERVER_NAME"] === "localhost" ?
+//    [
+//        "database" => [
+//            "host" => "localhost",
+//            "port" => 3306,
+//            "dbname" => "vps_sajt",
+//            "charset" => "utf8mb4",
+//            "username" => "root",
+//            "password" => ""
+//        ]] : [
+//        "database" => [
+//            "host" => "localhost",
+//            "port" => 3306,
+//            "dbname" => "vpscacak_vps_sajt",
+//            "charset" => "utf8mb4",
+//            "username" => "vpscacak_vps_admin",
+//            "password" => "R!1kQaJUi%U;"
+//        ]
+//    ];
