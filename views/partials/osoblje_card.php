2c0ec17a4996c22ebb1e5b1a6d35aaa26ab19940
zile028 <zile028@gmail.com>
2024-05-11 22:40:09 +0200

    fix style

--- views/partials/osoblje_card.php
+++ views/partials/osoblje_card.php
@@ -1,12 +1,15 @@
 <div class="card">
     <img src="<?php uploadPath($nastavnik->image); ?>" alt="">
     <div class="card-body">
-        <h4><?php echo $nastavnik->firstName; ?>
-            <br><?php echo $nastavnik->lastName; ?></h4>
-        <p><?php echo $nastavnik->rank; ?></p>
+        <div class="card-body_group">
 
-        <a class="email" href="mailto:<?php echo $nastavnik->email;
-        ?>"><?php echo $nastavnik->email; ?></a>
+            <h4><?php echo $nastavnik->firstName; ?>
+                <br><?php echo $nastavnik->lastName; ?></h4>
+            <p><?php echo $nastavnik->rank; ?></p>
+
+            <a class="email" href="mailto:<?php echo $nastavnik->email;
+            ?>"><?php echo $nastavnik->email; ?></a>
+        </div>
 
         <a href="nastavno_osoblje/<?php echo $nastavnik->id; ?>" class="btn
                         btn-sm">Детаљније</a>
