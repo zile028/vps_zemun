69db05fbc07a449fc5a3d2a57aab7fc165440677
Dejan Zivkovic <zile028@gmail.com>
2024-06-05 12:10:28 +0200

    Update cenovnik

--- controllers/onama/cenovnik.php
+++ controllers/onama/cenovnik.php
@@ -1,19 +1,22 @@
 <?php
 $db = \Core\App::resolve(\Core\Database::class);
 // $cenovnik = $db->query("SELECT * FROM cenovnik WHERE lang = 'srb'")->find(PDO::FETCH_OBJ);
-
-$db = \Core\App::resolve(\Core\Database::class);
 $sql = "SELECT k.category, d.* ,k.category,
+        CASE
+            WHEN d.parentID = 0 THEN d.id
+            ELSE d.parentID 
+        END AS parentID
         FROM dokumenta d
         JOIN kategorije k ON k.id = d.category
-        WHERE d.lang = :lang AND d.category = 6
-        ORDER BY k.category;
+        WHERE d.lang = :lang AND k.id = 6
+        ORDER BY k.category, parentID ASC;
         ";
 $params["lang"] = "srb";
-$cenovnik = $db->query($sql, $params)->find(PDO::FETCH_OBJ);
-dd($cenovnik);
+
 view("cenovnik.view", [
     "heroImage" => "hero_cenovnik.jpg",
     "heroTitle" => "Ценовник",
     "cenovnik" => $cenovnik
 ]);
+
+
