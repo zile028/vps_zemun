af0f293f88e5833013e2a8861b5866a4725f258f
Dejan Zivkovic <zile028@gmail.com>
2024-06-05 12:07:27 +0200

    Update cenovnik

--- controllers/onama/cenovnik.php
+++ controllers/onama/cenovnik.php
@@ -4,13 +4,9 @@ $db = \Core\App::resolve(\Core\Database::class);
 
 $db = \Core\App::resolve(\Core\Database::class);
 $sql = "SELECT k.category, d.* ,k.category,
-        CASE
-            WHEN d.parentID = 0 THEN d.id
-            ELSE d.parentID 
-        END AS parentID
         FROM dokumenta d
         JOIN kategorije k ON k.id = d.category
-        WHERE d.lang = :lang AND k.id = 7
+        WHERE d.lang = :lang AND k.id = 6
         ORDER BY k.category, parentID ASC;
         ";
 $params["lang"] = "srb";
