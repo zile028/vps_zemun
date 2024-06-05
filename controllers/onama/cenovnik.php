b4ce0d0347524cdf9ec39b667bcd8f6ee260d6fb
Dejan Zivkovic <zile028@gmail.com>
2024-06-05 12:08:18 +0200

    Update cenovnik

--- controllers/onama/cenovnik.php
+++ controllers/onama/cenovnik.php
@@ -6,8 +6,8 @@ $db = \Core\App::resolve(\Core\Database::class);
 $sql = "SELECT k.category, d.* ,k.category,
         FROM dokumenta d
         JOIN kategorije k ON k.id = d.category
-        WHERE d.lang = :lang AND k.id = 6
-        ORDER BY k.category, parentID ASC;
+        WHERE d.lang = :lang AND d.category = 6
+        ORDER BY k.category;
         ";
 $params["lang"] = "srb";
 $cenovnik = $db->query($sql, $params)->find(PDO::FETCH_OBJ);
