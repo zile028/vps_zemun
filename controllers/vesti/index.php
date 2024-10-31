56b8fdca3932d6c0adfa732a3758523668d06a51
Dejan Zivkovic <zile028@gmail.com>
2024-10-31 10:45:27 +0100

    Change sorting news to DESC

--- controllers/vesti/index.php
+++ controllers/vesti/index.php
@@ -4,7 +4,8 @@ $db = \Core\App::resolve(\Core\Database::class);
 $sql = "SELECT v.* ,m.fileName,m.storeName, m.id AS mediaID, u.role AS autor
         FROM vesti v 
         JOIN users u ON u.id = v.userID
-        LEFT JOIN media m ON m.id = v.featured_imageID;";
+        LEFT JOIN media m ON m.id = v.featured_imageID
+        ORDER BY v.createdAt DESC;";
 $vesti = $db->query($sql)->find(PDO::FETCH_OBJ);
 
 view("vesti.view", [
